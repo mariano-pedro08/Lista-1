722f4f5f3863c4f8507aec7819a79d45d5b1667a
Pedro Mariano Vieira Nolasco <marianopedro237@gmail.com>
2026-08-26 21:33:56 -0300

    Add files via upload

---
+++ Questão 2.cpp
@@ -0,0 +1,17 @@
+   #include <stdio.h>
+   #include <stdlib.h>
+   main(){
+//   cadastro
+         int n1, n2;
+         
+         printf("Digite sua n1:");
+          scanf("%i",&n1);
+          
+          printf("Digite sua n2:");
+          scanf("%i",&n2);
+//   Declarando a media das notas    
+    float media = (n1+n2)/2;
+    
+    printf("Sua média:", media );
+}
+   
