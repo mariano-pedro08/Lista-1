e21b1d875a835ad6bac0ac6d3b09280a67e1665c
Pedro Mariano Vieira Nolasco <marianopedro237@gmail.com>
2026-09-01 08:47:38 -0300

    Add files via upload

---
+++ questão 10.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main (){
+	
+    float raio, area;
+    
+    printf("Digite a area do circulo:");
+    scanf("%f",&raio);
+    
+     area= M_PI *pow(raio,2);
+
+    
+    printf("Area do circulo e: %.2f\n",area);
+	
+	
+	
+	
+}
